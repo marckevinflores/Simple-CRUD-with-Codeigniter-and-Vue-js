0dff08f39d1bb75f38498b5910961b67fd535e66
Marc Kevin Flores <marckevinflores@gmail.com>
2018-06-02 17:52:41 +0800

    Simple CRUD with Codeigniter and Vue JS
    Simple Codeigniter and Vue js CRUD
    
    Include:
    Bootstrap 4 - for design
    Bulma - for modal
    Animate CSS - for animation design
    Font Awesome - for cool icons
    Axios - for AJAX/HTTP library
    
    Functions:
    Create
    Read
    Edit
    Delete
    Search
    Pagination

---
+++ civuejs/application/views/template/footer.php
@@ -0,0 +1,5 @@
+</body>
+<script src="<?php echo base_url();?>/assets/js/pagination.js"></script>
+<script src="<?php echo base_url();?>/assets/js/app.js"></script>
+
+</html>
---
+++ civuejs/application/views/template/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Codeigniter + Vue JS</title>
+<link rel="icon" href="<?php echo base_url()?>assets/img/civue.png">
+ <link rel="stylesheet" href="<?php echo base_url()?>assets/css/bulma.min.css">
+ <link rel="stylesheet" href="<?php echo base_url()?>assets/css/bootstrap.min.css">
+ <link rel="stylesheet" href="<?php echo base_url()?>assets/css/animate.min.css">
+ <link rel="stylesheet" href="<?php echo base_url()?>assets/css/font-awesome.min.css">
+ <link rel="stylesheet" href="<?php echo base_url()?>assets/css/style.css">
+
+<script src="<?php echo base_url()?>assets/js/vue.min.js"></script>
+<script src="<?php echo base_url()?>assets/js/axios.min.js"></script>
+<script src="<?php echo base_url()?>assets/js/jquery.min.js"></script>
+
+  
+</head>
+<body class="bg-light">
